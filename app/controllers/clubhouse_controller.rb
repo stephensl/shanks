a88037267c00de6a5903067522e2da10e9816ba0
Lawton Stephens <stephens.lawton@gmail.com>
2024-02-23 23:56:25 -0500

    set up base controllers and crud operations

--- app/controllers/clubhouse_controller.rb
+++ app/controllers/clubhouse_controller.rb
@@ -1,5 +1,7 @@
 class ClubhouseController < ApplicationController
   before_action :authenticate_member!
   def index
+    @upcoming_rounds = current_member.rounds.where('date > ?', Time.now).order(date: :asc)
+    @past_rounds = current_member.rounds.where('date <= ?', Time.now).order(date: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :members, :controllers => { registrations: 'members/registrations'}
+
   root 'clubhouse#index'
+
+  resources :courses
+  resources :rounds
+  resources :member_rounds
+
 end
